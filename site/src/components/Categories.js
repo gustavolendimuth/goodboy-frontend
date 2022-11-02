d31491e202bc6a70e7dac902555c743ae3523ddd
Danilo de Barros Ribeiro <nilo22@gmail.com>
2022-11-02 14:07:26 -0300

    Update Categories.js

--- site/src/components/Categories.js
+++ site/src/components/Categories.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import fetchContent from '../services/fetchContent';
+import Context from '../context/Context';
+
+// Renderizar os botões de categorias
+// pegar o retorno da requisição da API
 
 export default function Categories() {
+  const {
+    categories,
+    setCategories,
+  } = useContext(Context);
+
+  getProducts = async () => {
+    const categoriesResponse = await fetchContent('categories');
+    setCategories(categoriesResponse);
+  };
+
   return (
-    <div>Categories</div>
+    <div>
+      Cães
+      {/* {map das subs dos cães} */}
+      Gatos
+      {/* {map das subs dos gatos} */}
+    </div>
   );
 }
