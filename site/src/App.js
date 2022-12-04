0910dc8ad11f67a67e79578aa6bc077497504230
Gabriela Ruas <gabrieladnr@gmail.com>
2022-12-04 12:15:22 -0300

    add css product details

--- site/src/App.js
+++ site/src/App.js
@@ -22,77 +22,77 @@ import fetchContent from './services/fetchContent';
 import formatCategoriesObject from './services/formatCategoriesObject';
 
 function App() {
-    const {
-        getLocalStorage,
-        setLocalStorage,
-        session,
-        setSession,
-    } = useContext(Context);
+  const {
+    getLocalStorage,
+    setLocalStorage,
+    session,
+    setSession,
+  } = useContext(Context);
 
-    useEffect(() => {
-        const localStorageSession = getLocalStorage('session');
-        if (!localStorageSession) {
-            setSession(window.crypto.randomUUID());
-            return;
-        }
-        setSession(localStorageSession);
-    }, []);
+  useEffect(() => {
+    const localStorageSession = getLocalStorage('session');
+    if (!localStorageSession) {
+      setSession(window.crypto.randomUUID());
+      return;
+    }
+    setSession(localStorageSession);
+  }, []);
 
-    useEffect(() => {
-        setLocalStorage('session', session);
-    }, [session]);
+  useEffect(() => {
+    setLocalStorage('session', session);
+  }, [session]);
 
-    const {
-        products,
-        setProducts,
-        categories,
-        setCategories,
-    } = useContext(Context);
+  const {
+    products,
+    setProducts,
+    categories,
+    setCategories,
+  } = useContext(Context);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
+  useEffect(() => {
+    window.scrollTo(0, 0);
 
-        const getProducts = async () => {
-            const productsResponse = await fetchContent('products');
+    const getProducts = async () => {
+      const productsResponse = await fetchContent('products');
 
-            if (productsResponse) setProducts(productsResponse);
-        };
+      if (productsResponse) setProducts(productsResponse);
+    };
 
-        const getCategories = async () => {
-            const categoriesResponse = await fetchContent('categories');
+    const getCategories = async () => {
+      const categoriesResponse = await fetchContent('categories');
 
-            if (categoriesResponse) setCategories(formatCategoriesObject(categoriesResponse));
-        };
+      if (categoriesResponse) setCategories(formatCategoriesObject(categoriesResponse));
+    };
 
-        getProducts();
-        getCategories();
-    }, []);
+    getProducts();
+    getCategories();
+  }, []);
 
-    useEffect(() => {
-        if (categories) console.log('Objeto das categorias', categories);
-        if (products) console.log('Objeto dos produtos', products);
-    }, [products, categories]);
+  useEffect(() => {
+    if (categories) console.log('Objeto das categorias', categories);
+    if (products) console.log('Objeto dos produtos', products);
+  }, [products, categories]);
 
-    return (
-        <BrowserRouter>
-            <header>
-                <Navbar />
-                <section>
-                    <Categories />
-                </section>
-            </header>
-            <main>
-                <Routes>
-                    <Route element={<Home />} path="/" exact />
-                    <Route element={<ProductDetails />} path="/produto/:id" />
-                    <Route element={<Products />} path="/produto/:query" />
-                    <Route element={<Cart />} path="/carrinho/:session" />
-                    <Route element={<Checkout />} path="/checkout/:session" />
-                </Routes>
-            </main>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <header>
+        <Navbar />
+        <section>
+          <Categories />
+        </section>
+      </header>
+      <main>
+        <Routes>
+          <Route element={ <Home /> } path="/" exact />
+          <Route element={ <ProductDetails /> } path="/produto/:id" />
+          <Route element={ <Products /> } path="/produto/:query" />
+          <Route element={ <Cart /> } path="/carrinho/:session" />
+          <Route element={ <Checkout /> } path="/checkout/:session" />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- site/src/pages/ProductDetails.js
+++ site/src/pages/ProductDetails.js
@@ -2,74 +2,73 @@ import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import Context from '../context/Context';
 import urlFor from '../services/urlFor';
+import '../css/productDetails.css';
 
 export default function ProductDetails() {
-    const {
-        products,
-        getLocalStorage,
-        setLocalStorage,
-        session,
-        setSession,
-    } = useContext(Context);
-    const { id } = useParams();
-    const [product, setProduct] = useState();
+  const {
+    products,
+    getLocalStorage,
+    setLocalStorage,
+    session,
+    setSession,
+  } = useContext(Context);
+  const { id } = useParams();
+  const [product, setProduct] = useState();
 
-    console.log({
-        getLocalStorage,
-        setLocalStorage,
-        session,
-        setSession
-    })
+  console.log({
+    getLocalStorage,
+    setLocalStorage,
+    session,
+    setSession,
+  });
 
-    useEffect(() => {
-        // console.log("produtos na pagina de detalhes", products)
-        const productsWithThisId = products?.filter((p) => p._id === id);
-        if (productsWithThisId?.length > 0) {
-            setProduct(productsWithThisId[0]);
-        }
-    }, [products, id]);
-
-    const handleAddClick = () => {
-        // check if this product is already in the cart
-        const shoppingCart = getLocalStorage('shoppingCart');
-        if (shoppingCart) {
-            if (shoppingCart[id]) {
-                shoppingCart[id] = shoppingCart[id] + 1;
-            } else {
-                shoppingCart[id] = 1;
-            }
-            setLocalStorage('shoppingCart', shoppingCart);
-        } else {
-            setLocalStorage('shoppingCart', { [id]: 1 });
-        }
+  useEffect(() => {
+    // console.log("produtos na pagina de detalhes", products)
+    const productsWithThisId = products?.filter((p) => p._id === id);
+    if (productsWithThisId?.length > 0) {
+      setProduct(productsWithThisId[0]);
     }
+  }, [products, id]);
 
-    if (product?.title) {
-        return (
-            <div>
-                <div>
-                    <div key={product.title}>
-                        <div>
-                            <div>
-                                <p>{product.title}</p>
-                            </div>
-                            <div>
-                                <img src={urlFor(product.photo?.image).url()} alt={product.photo?.alt} />
-                            </div>
-                            <div>
-                                <p>{product.description}</p>
-                            </div>
-                            <div>
-                                <div onClick={() => handleAddClick()}>QUANTIDADE E ADICIONAR NO CARRINHO</div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        );
+  const handleAddClick = () => {
+    // check if this product is already in the cart
+    const shoppingCart = getLocalStorage('shoppingCart');
+    if (shoppingCart) {
+      if (shoppingCart[id]) {
+        shoppingCart[id] = shoppingCart[id] + 1;
+      } else {
+        shoppingCart[id] = 1;
+      }
+      setLocalStorage('shoppingCart', shoppingCart);
     } else {
-        <div>Produto não encontrado</div>
+      setLocalStorage('shoppingCart', { [id]: 1 });
     }
+  };
 
+  if (product?.title) {
+    return (
+      <div>
+        <div>
+          <div className="product-title" key={ product.title }>
+            <div>
+              <div>
+                <p>{product.title}</p>
+              </div>
+              <div>
+                <img className="product-image" src={ urlFor(product.photo?.image).url() } alt={ product.photo?.alt } />
+              </div>
+              <div>
+                <p className="product-description">{product.description}</p>
+              </div>
+              <div>
+                <div onClick={ () => handleAddClick() }>QUANTIDADE E ADICIONAR NO CARRINHO</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+  <div>Produto não encontrado</div>;
 }
