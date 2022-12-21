dcca53b7614efded768086d5a6dbd28ba8f1bc20
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-12-20 22:15:23 -0300

    added cors to login

--- site/src/pages/Login.js
+++ site/src/pages/Login.js
@@ -38,6 +38,7 @@ function Login() {
   const getToken = async () => {
     const response = await fetch(`${process.env.REACT_APP_PROJECT_API_URL}/login`, {
       method: 'POST',
+      mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
       },
